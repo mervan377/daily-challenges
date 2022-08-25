3888eef3a6d848abc5f818bab4915799d9fe011c
Mervan Yalçın <mervan37ylcn@gmail.com>
2022-08-25 18:32:02 +0300

    Checked 4th Challenges

--- 04-find-missing-numbers-in-array/index.ts
+++ 04-find-missing-numbers-in-array/index.ts
@@ -1,19 +1,15 @@
 const findMissingNumber = (numbers: number[]) => {
-  let i = 0;
-
-  while (i < 10) {
-    for (let number in numbers) {
-      if (numbers[number] === i) {
-        console.log(number);
-        console.log("Triyng for Mr. Emin ")
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[j] === i) {
+        i = 9;
       }
     }
-    i++;
   }
 };
+findMissingNumber([2, 3, 4, 6, 9]);
 
-const numbers: number[] = [0, 1, 3, 4, 6, 9];
-
-// 0 ile 9 arasinda ki silinen sayiulari bul
+//const numbers: number[] = [0, 1, 3, 4, 6, 9];
+// 0 ile 9 arasinda ki silinen sayilari bul
 // method olacak example output [2, 5, 7, 8]
-// number array alacak number array d,necek
+// number array alacak number array dönecek
