d0a9890a9a1ed353912f73a5df0735ebcfa950b9
Mervan Yalçın <mervan37ylcn@gmail.com>
2022-08-30 10:59:04 +0300

    Finished 6th Challenge

--- 06-find-gcmandlcm-between-minandmax/index.ts
+++ 06-find-gcmandlcm-between-minandmax/index.ts
@@ -1,3 +1,12 @@
-
-
+const findGcmLcm = (min, max, number1, number2): number[] => {
+  let gcmlcmNumbers: number[] = [];
+  for (let i = min; i <= max; i++) {
+    if (i % (number1 * number2) === 0) {
+      gcmlcmNumbers.push(i);
+    }
+  }
+  return gcmlcmNumbers;
+};
+const findGcmLcmNumbers: number[] = findGcmLcm(0, 200, 3, 5);
+console.log(findGcmLcmNumbers);
 //İki sayı arasında ki dışarıdan gelen rastgele iki sayının okek ve obebini bulan fonksiyon
